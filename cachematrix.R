75edce48b717e9ed71e5a86a1301f36be1e1ae38
Andrea Naspi <andreanaspi@gmail.com>
2018-08-17 21:52:49 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## the make cache function to save the matrix in environment with the associated setter/getter matrix and 
+## setter/getter mean
+makeCacheMatrix <- function(x = numeric()) {
+  ##matrix field
+  m <- NULL
+  
+  ##set matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  ##get matrix
+  get <- function() x
+  
+  ##set mean
+  setmean <- function(mean) m <<- mean
+  
+  ##get mean
+  getmean <- function() m
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  ##return a list of the upper function
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##calculate the mean of the special list created in the above function
+cachemean <- function(x, ...) {
+  
+  ##if the value is already in cache return 
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ##otherwise calculate the mean and set in cache
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  
+  ##return
+  m
 }
